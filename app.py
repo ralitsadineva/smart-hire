8cea5f95d9c31466f28bc9926e41e0158aa55a58
DESKTOP-MGFAPSA\ralitsa.dineva <ralitsa.dineva@imperiaonline.org>
2023-05-31 18:03:40 +0300

    log in and sign up pages

--- app.py
+++ app.py
@@ -1,20 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import re
 
 app = Flask(__name__)
+
+def is_valid_password(input_str):
+    # Check if the input matches the pattern
+    pattern = r"^(?=.*[0-9])(?=.*[\W_])(?!.*\s)[A-Za-z0-9\W_]{6,}$"
+    
+    return bool(re.match(pattern, input_str))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    #return render_template('login.html')
-    pass
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        #check if in database
+        return 'Got it!' #redirect('/home.html')
+    else:
+        return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    #return render_template('signup.html')
-    pass
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        #check if valid according to rules
+        invalid = False
+        if is_valid_password(password):
+            pass #try to add to database
+        else:
+            invalid = True
+            return render_template('signup.html', invalid=invalid)
+        #add to database
+        return 'Got it!' #redirect('/home.html')
+    else:
+        return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
