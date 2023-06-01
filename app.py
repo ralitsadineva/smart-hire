f58dad75d1d132bfd2d653803d7f49624a6c40ae
DESKTOP-MGFAPSA\ralitsa.dineva <ralitsa.dineva@imperiaonline.org>
2023-06-01 17:30:45 +0300

    add users to database, check if users exist

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
 from flask import Flask, render_template, request, redirect
 import re
+import psycopg2
+from configparser import ConfigParser
+import bcrypt
+
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    return db
+
+params = config()
+
+conn = psycopg2.connect(**params)
+print(conn)
+
+cursor = conn.cursor()
+print(cursor)
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL NOT NULL PRIMARY KEY, 
+        email VARCHAR(255) NOT NULL UNIQUE, 
+        username VARCHAR(255) NOT NULL UNIQUE, 
+        password VARCHAR(255) NOT NULL
+    );
+    """)
+conn.commit()
 
 app = Flask(__name__)
 
 def is_valid_password(input_str):
     # Check if the input matches the pattern
     pattern = r"^(?=.*[0-9])(?=.*[\W_])(?!.*\s)[A-Za-z0-9\W_]{6,}$"
-    
     return bool(re.match(pattern, input_str))
 
 @app.route('/')
@@ -19,6 +55,11 @@ def login():
         username = request.form['username']
         password = request.form['password']
         #check if in database
+        cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s;", (username, password))
+        user = cursor.fetchone()
+        print(user)
+        if user is None:
+            return render_template('login.html', invalid=True)
         return 'Got it!' #redirect('/home.html')
     else:
         return render_template('login.html')
@@ -30,14 +71,23 @@ def signup():
         username = request.form['username']
         password = request.form['password']
         #check if valid according to rules
-        invalid = False
+        #invalid = False
+        #exist=False
         if is_valid_password(password):
-            pass #try to add to database
+            try:
+                cursor.execute("""
+                    INSERT INTO users (email, username, password)
+                    VALUES (%s, %s, %s);
+                    """, (email, username, password))
+                conn.commit()
+                return 'You registered succesfully!' #redirect('/home.html')
+            except (Exception, psycopg2.DatabaseError) as error:
+                print(error)
+                conn.rollback()
+                return render_template('signup.html', exist=True)
         else:
-            invalid = True
-            return render_template('signup.html', invalid=invalid)
-        #add to database
-        return 'Got it!' #redirect('/home.html')
+            #invalid = True
+            return render_template('signup.html', invalid=True)
     else:
         return render_template('signup.html')
 
