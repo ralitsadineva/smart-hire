99dfcbd94444a52f2a20db80d3ac31b0439aed15
DESKTOP-MGFAPSA\ralitsa.dineva <ralitsa.dineva@imperiaonline.org>
2023-06-06 18:28:14 +0300

    added flask sessions, profile page and positions page with the option to add, view and edit positions

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
-from database import create_tables, UniqueViolationError, DatabaseError, insert_user, get_user, update_password
+from flask import Flask, render_template, request, redirect, session
+from database import create_tables, UniqueViolationError, DatabaseError, insert_user, get_user, update_password, create_positions_table, get_positions, insert_position, get_position, update_position
 from validation import is_valid_username, is_valid_password
 import bcrypt
 import logging
+import secrets
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -12,6 +13,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 
 @app.route('/')
 def index():
@@ -26,7 +28,10 @@ def login():
         user = get_user(username)
         if user is None or not bcrypt.checkpw(password.encode(), bytes(user[3])):
             return render_template('login.html', invalid=True)
-        return 'Got it!' #redirect('/home.html')
+        session['username'] = username
+        session['user_id'] = user[0]
+        session['email'] = user[1]
+        return redirect('/home')
     else:
         return render_template('login.html')
 
@@ -42,7 +47,7 @@ def signup():
                 hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
                 try:
                     insert_user(email, username, hashed_password)
-                    return 'You registered succesfully!' #redirect('/home.html')
+                    return redirect('/login')
                 except UniqueViolationError as e:
                     logger.error(f"{type(e)}\n{e}")
                     return render_template('signup.html', exist=True) 
@@ -58,24 +63,24 @@ def signup():
 
 @app.route('/logout')
 def logout():
-    #session.pop('username', None)
+    session.pop('username', None)
     return redirect('/')
 
 @app.route('/change_password', methods=['GET', 'POST'])
 def change_password():
-    # if 'username' not in session:
-    #     return redirect('/')
+    if 'username' not in session:
+        return redirect('/')
     if request.method == 'POST':
         old_password = request.form['old_password']
         new_password = request.form['new_password']
-        user = get_user(username) #session['username']
+        user = get_user(session['username'])
         if not bcrypt.checkpw(old_password.encode(), bytes(user[3])):
             return render_template('change_password.html', invalid=True)
         if is_valid_password(new_password):
             hashed_password = bcrypt.hashpw(new_password.encode(), bcrypt.gensalt())
             try:
-                update_password(hashed_password, username) #session['username']
-                return 'You changed your password succesfully!' #redirect('/home.html')
+                update_password(hashed_password, session['username'])
+                return redirect('/login')
             except DatabaseError as error:
                 logger.error(f"{type(error)}\n{error}")
                 return render_template('change_password.html', error=True)
@@ -86,10 +91,62 @@ def change_password():
 
 @app.route('/home')
 def home():
-    # if 'username' not in session:
-    #     return redirect('/')
+    if 'username' not in session:
+        return redirect('/')
     return render_template('home.html')
+
+@app.route('/profile')
+def profile():
+    if 'username' not in session:
+        return redirect('/')
+    username = session['username']
+    email = session['email']
+    return render_template('profile.html', username=username, email=email)
+
+@app.route('/positions')
+def positions():
+    if 'username' not in session:
+        return redirect('/')
+    positions = get_positions(session['user_id'])
+    return render_template('positions.html', positions=positions)
+
+@app.route('/positions/<string:position_id>')
+def position(position_id):
+    if 'username' not in session:
+        return redirect('/')
+    position = get_position(position_id)
+    return render_template('position.html', position=position)
+
+@app.route('/positions/<string:position_id>/edit', methods=['GET', 'POST'])
+def edit_position(position_id):
+    if 'username' not in session:
+        return redirect('/')
+    if request.method == 'POST':
+        try:
+            update_position(position_id, request.form['title'], request.form['description'])
+            return redirect('/positions')
+        except DatabaseError as error:
+            logger.error(f"{type(error)}\n{error}")
+            return render_template('edit_position.html', error=True)
+    else:
+        position = get_position(position_id)
+        return render_template('edit_position.html', position=position)
+
+@app.route('/positions/add', methods=['GET', 'POST'])
+def add_position():
+    if 'username' not in session:
+        return redirect('/')
+    if request.method == 'POST':
+        try:
+            insert_position(session['user_id'], request.form['title'], request.form['description'])
+            return redirect('/positions')
+        except DatabaseError as error:
+            logger.error(f"{type(error)}\n{error}")
+            return render_template('add_position.html', error=True)
+    else:
+        return render_template('add_position.html')
 
 if __name__ == '__main__':
     create_tables()
+    create_positions_table()
     app.run(debug=True, host='0.0.0.0')
--- database.py
+++ database.py
@@ -54,3 +54,92 @@ def update_password(password, username):
     except (Exception, psycopg2.DatabaseError) as error:
         conn.rollback()
         raise DatabaseError(error)
+
+def create_positions_table():
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS positions (
+            pos_id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(), 
+            user_id UUID NOT NULL REFERENCES users (id), 
+            title VARCHAR(255) NOT NULL, 
+            description VARCHAR NOT NULL, 
+            active BOOLEAN NOT NULL DEFAULT TRUE, 
+            created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            last_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+    conn.commit()
+
+def insert_position(user_id, title, description):
+    try:
+        cursor.execute("""
+            INSERT INTO positions (user_id, title, description)
+            VALUES (%s, %s, %s);
+            """, (user_id, title, description))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        conn.rollback()
+        raise DatabaseError(error)
+
+def get_positions(user_id):
+    cursor.execute("SELECT * FROM positions WHERE user_id = %s;", (user_id, ))
+    return cursor.fetchall()
+
+def get_position(pos_id):
+    cursor.execute("SELECT * FROM positions WHERE pos_id = %s;", (pos_id, ))
+    return cursor.fetchone()
+
+def make_position_inactive(pos_id):
+    try:
+        cursor.execute("""
+            UPDATE positions
+            SET active = FALSE, last_updated = CURRENT_TIMESTAMP
+            WHERE pos_id = %s;
+            """, (pos_id, ))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        conn.rollback()
+        raise DatabaseError(error)
+
+def make_position_active(pos_id):
+    try:
+        cursor.execute("""
+            UPDATE positions
+            SET active = TRUE, last_updated = CURRENT_TIMESTAMP
+            WHERE pos_id = %s;
+            """, (pos_id, ))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        conn.rollback()
+        raise DatabaseError(error)
+
+def update_position(pos_id, title, description):
+    try:
+        cursor.execute("""
+            UPDATE positions
+            SET title = %s, description = %s, last_updated = CURRENT_TIMESTAMP
+            WHERE pos_id = %s;
+            """, (title, description, pos_id))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        conn.rollback()
+        raise DatabaseError(error)
+
+def create_candidates_table():
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS candidates (
+            cand_id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(), 
+            pos_id UUID NOT NULL REFERENCES positions (pos_id), 
+            first_name VARCHAR(255) NOT NULL, 
+            last_name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL, 
+            phone_number VARCHAR(255), 
+            address VARCHAR(255),
+            postal_code VARCHAR(255),
+            city VARCHAR(255),
+            country VARCHAR(255),
+            date_of_birth DATE,
+            created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            last_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        );
+        """)
+    conn.commit()
