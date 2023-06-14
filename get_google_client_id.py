22f99c61f9d1511ce32394eef0d476c890c1ecb0
DESKTOP-MGFAPSA\ralitsa.dineva <ralitsa.dineva@imperiaonline.org>
2023-06-14 09:45:35 +0300

    removed the prints

--- get_google_client_id.py
+++ get_google_client_id.py
@@ -2,9 +2,7 @@ import json
 
 def get_google_client_id():
     client_secret = json.load(open('client_secret.json'))
-    print(client_secret['web'])
-    print(client_secret['web']['client_id'])
     return client_secret['web']['client_id']
 
 if __name__ == '__main__':
-    get_google_client_id()
+    print(get_google_client_id())
